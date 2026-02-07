5eb51f9e1945d4399a52736b84a6a21c850e6f05
tobacopsg <vuhaihoang.yeuem@gmail.com>
2026-02-07 18:24:46 +0700

    Update db.py

--- db.py
+++ db.py
@@ -8,7 +8,19 @@ async def init_db():
         CREATE TABLE IF NOT EXISTS users (
             user_id INTEGER PRIMARY KEY,
             balance INTEGER DEFAULT 0,
-            referrals INTEGER DEFAULT 0
+            bank_name TEXT,
+            bank_stk TEXT,
+            bank_owner TEXT
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS deposit_bank (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            bank_name TEXT,
+            bank_stk TEXT,
+            bank_owner TEXT,
+            content TEXT
         )
         """)
         await db.commit()
@@ -28,4 +40,36 @@ async def add_balance(uid, amount):
     async with aiosqlite.connect(DB_NAME) as db:
         await db.execute("UPDATE users SET balance = balance + ? WHERE user_id=?", (amount, uid))
         await db.commit()
+
+async def set_bank(uid, bank, stk, owner):
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute(
+            "UPDATE users SET bank_name=?, bank_stk=?, bank_owner=? WHERE user_id=?",
+            (bank, stk, owner, uid)
+        )
+        await db.commit()
+
+async def get_bank(uid):
+    async with aiosqlite.connect(DB_NAME) as db:
+        cur = await db.execute(
+            "SELECT bank_name, bank_stk, bank_owner FROM users WHERE user_id=?",
+            (uid,)
+        )
+        return await cur.fetchone()
+
+async def set_deposit_bank(bank, stk, owner, content):
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute("DELETE FROM deposit_bank")
+        await db.execute("""
+            INSERT INTO deposit_bank (bank_name, bank_stk, bank_owner, content)
+            VALUES (?, ?, ?, ?)
+        """, (bank, stk, owner, content))
+        await db.commit()
+
+async def get_deposit_bank():
+    async with aiosqlite.connect(DB_NAME) as db:
+        cur = await db.execute("SELECT bank_name, bank_stk, bank_owner, content FROM deposit_bank LIMIT 1")
+        return await cur.fetchone()
+
+
 
