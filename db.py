be9b144b79ac6c0d5d0c9405a1d0d68f6532406b
tobacopsg <vuhaihoang.yeuem@gmail.com>
2026-02-08 06:54:19 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,24 @@
-import sqlite3
+import sqlite3, time
 
 conn = sqlite3.connect("data.db", check_same_thread=False)
 cur = conn.cursor()
 
-def init_db():
-    cur.execute("""CREATE TABLE IF NOT EXISTS users(
-        user_id INTEGER PRIMARY KEY,
-        balance INTEGER DEFAULT 1000
-    )""")
-    conn.commit()
+cur.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    user_id INTEGER PRIMARY KEY,
+    balance INTEGER DEFAULT 0,
+    last_daily INTEGER DEFAULT 0
+)
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS giftcodes(
+    code TEXT PRIMARY KEY,
+    used INTEGER DEFAULT 0
+)
+""")
+
+conn.commit()
 
 def get_user(uid):
     cur.execute("SELECT * FROM users WHERE user_id=?", (uid,))
@@ -16,13 +26,14 @@ def get_user(uid):
         cur.execute("INSERT INTO users(user_id) VALUES(?)", (uid,))
         conn.commit()
 
-def get_balance(uid):
-    cur.execute("SELECT balance FROM users WHERE user_id=?", (uid,))
-    return cur.fetchone()[0]
-
-def set_balance(uid, bal):
-    cur.execute("UPDATE users SET balance=? WHERE user_id=?", (bal, uid))
+def add(uid, amount):
+    cur.execute("UPDATE users SET balance = balance + ? WHERE user_id=?", (amount, uid))
     conn.commit()
 
+def sub(uid, amount):
+    cur.execute("UPDATE users SET balance = balance - ? WHERE user_id=?", (amount, uid))
+    conn.commit()
 
-
+def balance(uid):
+    cur.execute("SELECT balance FROM users WHERE user_id=?", (uid,))
+    return cur.fetchone()[0]
