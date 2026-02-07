bd7f006909a4362f7d5d74ae6870a3b28480602c
tobacopsg <vuhaihoang.yeuem@gmail.com>
2026-02-07 22:50:47 +0700

    Delete core directory

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-def admin_menu():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("💳 Cập nhật ngân hàng", callback_data="admin_bank")],
-        [InlineKeyboardButton("📊 Xem thống kê", callback_data="admin_stats")]
-    ])
